f556f78cda2178700f4ce95c9ef0f7e2106733de
geallenboy <gejialun88@163.com>
2022-01-20 14:48:07 +0800

    feat: code

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+declare const javascriptLint: any;
+declare const prettierLint: any;
+declare const reactLint: any;
+declare const styleLint: any;
+declare const typescriptLint: any;
+declare const vueLint: any;
